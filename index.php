98613749bf1d000b7ece53c9cca9c3938dca2ccc
jatibamoza <jatibamoza@gmail.com>
2025-09-30 16:15:32 +0200

    ajustes

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-header("Location: ejemploGeneraPet.php");
+<?php
+echo "<h1>Servidor Redsys Test</h1>";
+echo "<ul>";
+echo "<li><a href='ejemploGeneraPet.php'>Generar parámetros</a></li>";
+echo "<li><a href='ejemploRecepcionaPet.php'>Receptor notificación</a></li>";
+echo "</ul>";
 ?>
