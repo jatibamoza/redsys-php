01918cd0fb27efd4aa4a94c3363dcb8c6010d702
jatibamoza <jatibamoza@gmail.com>
2025-10-10 11:41:58 +0200

    Ajuste para redirección automática en generarPet.php

--- generarPet.php
+++ generarPet.php
@@ -40,14 +40,40 @@ $params = Utils::base64_url_encode_safe(json_encode($data));
 $signature = Signature::createMerchantSignature($kc, $params, $order);
 
 echo phpversion();
+$target = 'https://sis-t.redsys.es:25443/sis/realizarPago';
 ?>
 <html lang="es">
 	<body>
-		<form name="frm" action="https://sis-t.redsys.es:25443/sis/realizarPago" method="POST" target="_blank">
-			Ds_Merchant_SignatureVersion <input type="text" name="Ds_SignatureVersion" value="<?php echo $version; ?>"/></br>
-			Ds_Merchant_MerchantParameters <input type="text" name="Ds_MerchantParameters" value="<?php echo $params; ?>"/></br>
-			Ds_Merchant_Signature <input type="text" name="Ds_Signature" value="<?php echo $signature; ?>"/></br>
-			<input type="submit" value="Enviar" >
-		</form>
+        <!-- JATC: se elimina el tag target="_blank" -->
+		<form name="frm" action="<?php echo htmlspecialchars($target, ENT_QUOTES, 'UTF-8'); ?>" method="POST" accept-charset="UTF-8">
+		<!--
+        Ds_Merchant_SignatureVersion <input type="text" name="Ds_SignatureVersion" value="<?php echo $version; ?>"/></br>
+		Ds_Merchant_MerchantParameters <input type="text" name="Ds_MerchantParameters" value="<?php echo $params; ?>"/></br>
+		Ds_Merchant_Signature <input type="text" name="Ds_Signature" value="<?php echo $signature; ?>"/></br>
+		<input type="submit" value="Enviar" >
+        -->
+        <?php
+            foreach ($mapped as $k => $v) {
+                $name = htmlspecialchars($k, ENT_QUOTES, 'UTF-8');
+                $value = htmlspecialchars($v, ENT_QUOTES, 'UTF-8');
+                echo '<input type="hidden" name="'.$name.'" value="'.$value.'">', PHP_EOL;
+            }
+            ?>
+            <noscript>
+            <p>Haga clic en Continuar para seguir con el pago.</p>
+            <button type="submit">Continuar</button>
+            </noscript>
+        </form>
+        <script>
+            (function(){
+            try {
+                document.getElementById('fwd').submit();
+            } catch (e) {
+                // Si algo falla, deja el botón del <noscript>
+                console.warn('Auto-submit falló:', e);
+            }
+            })();
+        </script>
+        </form>
 	</body>
 </html>
